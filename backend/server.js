ac3323dc4f9b8ae64fb55af04c17e95b2d96743d
udaramadhuranga <udaramadhu128@gmail.com>
2021-08-15 20:49:17 +0530

    error corrected in server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ app.use(cors());
 
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended: true}));
+const productRouter = require('./routes/productRoutes')
+
 
 
 const URL = process.env.MONGODB_URL;
